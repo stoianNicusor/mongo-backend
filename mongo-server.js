5660c26fa284b12a1e8fa53f0b16e39a3be0cecb
Stoian Nicusor <nicusor@multidimension.ro>
2025-09-29 17:35:09 +0300

    Changed the order

--- mongo-server.js
+++ mongo-server.js
@@ -8,13 +8,16 @@ const cors = require('cors');
 const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
 
 app.use(cors({
   origin: 'https://cryptoprojectredux.netlify.app',
   credentials: true  
 }));
+
+app.use(express.json());
+app.use(cookieParser());
+
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
